fe318dd4706c438ffb5ad2d67b3fdc10483e99ee
Neven Dyulgerov <neven.diulgerov@gmail.com>
2025-10-15 22:03:14 +0300

    feat: Add types for products

---
+++ src/components/products/types.ts
@@ -0,0 +1,45 @@
+export interface ProductModel {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  tags: string[];
+  brand: string;
+  sku: string;
+  weight: number;
+  dimensions: ProductDimensionsModel;
+  warrantyInformation: string;
+  shippingInformation: string;
+  availabilityStatus: string;
+  images: string[];
+  thumbnail: string;
+  reviews: ProductReviewModel[];
+  returnPolicy: string;
+  minimumOrderQuantity: string;
+  meta: ProductMetaModel;
+}
+
+export interface ProductMetaModel {
+  createdAt: string;
+  updatedAt: string;
+  barcode: string;
+  qrCode: string;
+}
+
+export interface ProductReviewModel {
+  rating: number;
+  comment: string;
+  date: string;
+  reviewerName: string;
+  reviewerEmail: string;
+}
+
+export interface ProductDimensionsModel {
+  width: number;
+  height: number;
+  depth: number;
+}

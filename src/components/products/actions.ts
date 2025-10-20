3fc8afe24127c1a3dd75bcd6a6ddc0c740c71ab9
Neven Dyulgerov <neven.diulgerov@gmail.com>
2025-10-20 14:27:44 +0300

    feat: Add product actions

--- src/components/products/actions.ts
+++ src/components/products/actions.ts
@@ -1,7 +1,23 @@
+"use server";
+
 import type { ProductModel } from "@/components/products/types";
 
+/**
+ * @description Gets all available products from the API
+ * @returns ProductModel[]
+ */
 export const getProducts = async (): Promise<ProductModel[]> => {
   const result = await fetch("https://dummyjson.com/products");
   const data: { products: ProductModel[] } = await result.json();
   return data.products;
+};
+
+/**
+ * @description Gets a single product by given id from the API
+ * @param id
+ * @returns ProductModel
+ */
+export const getProduct = async (id: number): Promise<ProductModel> => {
+  const result = await fetch(`https://dummyjson.com/products/${id}`);
+  return (await result.json()) as ProductModel;
 };
